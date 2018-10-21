362788423bafcad84f67b2312f0cd10cb9c747cf
NGUYEN TIEN THANH <nguyentienthanh@NGUYENs-MBP.net.fpt>
2018-10-21 16:43:28 +0700

    init project

---
+++ src/main/java/vn/edu/tto/ThcsttoApplication.java
@@ -0,0 +1,12 @@
+package vn.edu.tto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThcsttoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThcsttoApplication.class, args);
+	}
+}
---
+++ src/test/java/vn/edu/tto/ThcsttoApplicationTests.java
@@ -0,0 +1,16 @@
+package vn.edu.tto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThcsttoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
