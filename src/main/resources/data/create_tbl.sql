98dac1830fc782331500a5012da9653418fac4a2
nguyentienthanh <nguyentienthanh@125.235.4.59.adsl.viettel.vn>
2018-10-28 23:17:39 +0700

    Update

--- src/main/resources/data/create_tbl.sql
+++ src/main/resources/data/create_tbl.sql
@@ -1,25 +1,128 @@
+-- Table: public.che_result
+
+-- DROP TABLE public.che_result;
+
+CREATE TABLE public.che_result
+(
+    id bigint NOT NULL DEFAULT nextval('che_result_id_seq'::regclass),
+    user_id bigint,
+    self_point double precision,
+    result_type character(100) COLLATE pg_catalog."default",
+    status character varying(100) COLLATE pg_catalog."default",
+    month integer,
+    updated_at timestamp without time zone,
+    leader_point double precision,
+    principal_point double precision,
+    leader_comment text COLLATE pg_catalog."default",
+    principal_comment text COLLATE pg_catalog."default",
+    leader_updated_at timestamp without time zone,
+    principal_updated_at timestamp without time zone,
+    self_created_at timestamp without time zone,
+    year integer,
+    leader_id bigint,
+    principal_id bigint,
+    CONSTRAINT che_result_pkey PRIMARY KEY (id),
+    CONSTRAINT che_result_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public."user" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.che_result
+    OWNER to postgres;
+    
+    -- Table: public.che_submit
+
+-- DROP TABLE public.che_submit;
+
+CREATE TABLE public.che_submit
+(
+    id bigint NOT NULL DEFAULT nextval('che_submit_id_seq'::regclass),
+    user_id bigint,
+    question_id bigint,
+    issue text COLLATE pg_catalog."default",
+    self_point double precision,
+    principal_point double precision,
+    final_point double precision,
+    month integer,
+    updated_at timestamp without time zone,
+    leader_point double precision,
+    year integer,
+    CONSTRAINT che_submit_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.che_submit
+    OWNER to postgres;
+    -- Table: public.question
+
+-- DROP TABLE public.question;
+
+CREATE TABLE public.question
+(
+    id bigint NOT NULL DEFAULT nextval('question_id_seq'::regclass),
+    index integer,
+    index_str character varying(100) COLLATE pg_catalog."default",
+    content text COLLATE pg_catalog."default",
+    max_point double precision,
+    is_increase boolean,
+    question_role character varying(100) COLLATE pg_catalog."default",
+    role_id bigint NOT NULL,
+    start_point double precision DEFAULT 1,
+    "group" text COLLATE pg_catalog."default",
+    CONSTRAINT question_pkey PRIMARY KEY (id),
+    CONSTRAINT question_role_id_fkey FOREIGN KEY (role_id)
+        REFERENCES public.role (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.question
+    OWNER to postgres;
+    -- Table: public.role
+
 -- DROP TABLE public.role;
 
 CREATE TABLE public.role
 (
-    id bigserial NOT NULL,
-    code character varying(100),
-    name text,
-    description text,
+    id bigint NOT NULL DEFAULT nextval('role_id_seq'::regclass),
+    code character varying(100) COLLATE pg_catalog."default",
+    name text COLLATE pg_catalog."default",
+    description text COLLATE pg_catalog."default",
     CONSTRAINT role_pkey PRIMARY KEY (id)
-);
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.role
+    OWNER to postgres;
+    
+    -- Table: public."user"
 
 -- DROP TABLE public."user";
 
 CREATE TABLE public."user"
 (
-    id bigserial NOT NULL,
-    user_name character varying(100),
-    password character varying(100),
-    first_name character varying(100),
-    last_name character varying(100),
-    team character varying(100),
-    "position" text,
+    id bigint NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+    user_name character varying(100) COLLATE pg_catalog."default",
+    password character varying(100) COLLATE pg_catalog."default",
+    first_name character varying(100) COLLATE pg_catalog."default",
+    last_name character varying(100) COLLATE pg_catalog."default",
+    team character varying(100) COLLATE pg_catalog."default",
+    "position" text COLLATE pg_catalog."default",
     is_team_leader boolean,
     role_id bigint,
     CONSTRAINT user_pkey PRIMARY KEY (id),
@@ -27,91 +130,11 @@ CREATE TABLE public."user"
         REFERENCES public.role (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-);
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
 
--- Table: public.che_result
-
--- DROP TABLE public.che_result;
-
-CREATE TABLE public.che_result
-(
-    id bigserial NOT NULL,
-    user_id bigint,
-    self_point integer,
-    result_type character(100),
-    status character varying(100),
-    month integer,
-    updated_at timestamp without time zone,
-    leader_point integer,
-    principal_point integer,
-    leader_comment text,
-    principal_comment text,
-    leader_updated_at timestamp without time zone,
-    principal_updated_at timestamp without time zone,
-    self_created_at timestamp without time zone,
-    year integer,
-    CONSTRAINT che_result_pkey PRIMARY KEY (id),
-    CONSTRAINT che_result_user_id_fkey FOREIGN KEY (user_id)
-        REFERENCES public."user" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-);
-
-
--- DROP TABLE public.che_submit;
-
-CREATE TABLE public.che_submit
-(
-    id bigserial NOT NULL,
-    user_id bigint,
-    question_id bigint,
-    issue text,
-    self_point integer,
-    lead_point integer,
-    principal_point integer,
-    final_point integer,
-    month integer,
-    updated_at timestamp without time zone,
-    CONSTRAINT che_submit_pkey PRIMARY KEY (id)
-);
-
--- DROP TABLE public.question;
-
-CREATE TABLE public.question
-(
-    id bigserial NOT NULL,
-    index integer,
-    index_str character varying(100),
-    content text,
-    max_point bigint,
-    is_increase boolean,
-    question_role character varying(100),
-    role_id bigint NOT NULL,
-    start_point integer DEFAULT 1,
-    "group" text,
-    CONSTRAINT question_pkey PRIMARY KEY (id),
-    CONSTRAINT question_role_id_fkey FOREIGN KEY (role_id)
-        REFERENCES public.role (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-);
-
-
--- insert role
-insert into "role" values(1, 'PRINCIPAL', 'Hiệu trưởng', null);
-insert into "role" values(2, 'VICE_PRINCIPAL', 'Hiệu phó', null);
-insert into "role" values(3, 'TEACHER', 'Giáo viên', null);
-insert into "role" values(4, 'EMPLOYEE', 'Nhân viên', null);
-insert into "role" values(5, 'DIRECTOR', 'Sở giáo dục', null);
-
-
--- insert user
-insert into "user" values(1, 'hieutruong', '$2a$10$RlMbzy0x40U6t8IohrfF4.2jwsTuoP1SrcHK9TkNv.B48skjTU1aO', 'Hiệu', 'Trưởng', '', 'Hiệu trưởng', false, 1);
-insert into "user" values(2, 'hieupho', '$2a$10$RlMbzy0x40U6t8IohrfF4.2jwsTuoP1SrcHK9TkNv.B48skjTU1aO', 'Hiệu', 'Phó', '', 'Hiệu phó', false, 2);
-insert into "user" values(3, 'giaovien', '$2a$10$RlMbzy0x40U6t8IohrfF4.2jwsTuoP1SrcHK9TkNv.B48skjTU1aO', 'Giáo', 'Viên', '1', 'Giáo viên', false, 3);
-insert into "user" values(4, 'nhanvien', '$2a$10$RlMbzy0x40U6t8IohrfF4.2jwsTuoP1SrcHK9TkNv.B48skjTU1aO', 'Nhân', 'Viên', '2', 'Nhân viên', false, 4);
-insert into "user" values(5, 'director', '$2a$10$RlMbzy0x40U6t8IohrfF4.2jwsTuoP1SrcHK9TkNv.B48skjTU1aO', 'Sở', 'Giáo Dục', '', 'Sở giáo dục', false, 5);
-insert into "user" values(6, 'totruonggiaovien', '$2a$10$RlMbzy0x40U6t8IohrfF4.2jwsTuoP1SrcHK9TkNv.B48skjTU1aO', 'Tổ Trưởng', 'Giáo Viên', '1', 'Tổ trưởng giáo viên', true, 3);
-insert into "user" values(7, 'totruongnhanvien', '$2a$10$RlMbzy0x40U6t8IohrfF4.2jwsTuoP1SrcHK9TkNv.B48skjTU1aO', 'Tổ Trưởng', 'Nhân Viên', '2', 'Tổ trưởng nhân viên', false, 4);
-
-
+ALTER TABLE public."user"
+    OWNER to postgres;
